2312e71f6f6c66531273985a754ccba714da79dc
Kamal Kiran Nethala <kamalkiran810@gmail.com>
2021-10-18 17:14:47 +0530

    Update1

---
+++ BooksApi/BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using BooksApi.Models;
+using BooksApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        [HttpGet]
+        public  Task<IEnumerable<Book>> GetBook()
+        {
+            return  _bookRepository.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return  await _bookRepository.Get(id);
+        }
+    }
+}
