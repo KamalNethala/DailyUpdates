74b2cb9dbc5f526c4dc1a8771f1a493aee628598
Kamal Kiran Nethala <kamalkiran810@gmail.com>
2021-10-19 19:13:34 +0530

    19-10-2021 update

---
+++ BooksApi/BooksApi/Repositories/BookRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using BooksApi.Repositories;
+using BooksApi.Models;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly string _connectionString;
+
+        public BookRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            var sqlQuery = "SELECT * FROM Books";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return await connection.QueryAsync<Book>(sqlQuery);
+            }
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            var sqlQuery = "SELECT * FROM Books WHERE Id=@BookId";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return await connection.QueryFirstOrDefaultAsync<Book>(sqlQuery, new { BookId = id });
+            }
+        }
+
+        public async Task<Book> Create(Book book)
+        {
+            var sqlQuery = "INSERT INTO Books(Title, Author, Description) VALUES (@Title, @Author, @Overview)";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new
+                {
+                    book.Title,
+                    book.Author,
+                    book.Overview
+                });
+
+                return book;
+            }
+        }
+
+        public async Task Update(Book book)
+        {
+            var sqlQuery = "UPDATE Books SET Title=@Title, Author=@Author, Overview=@Overview WHERE Id=@Id";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new
+                {
+                    book.Id,
+                    book.Title,
+                    book.Author,
+                    book.Overview
+                });
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var sqlQuery = "DELETE from Books WHERE Id=@id";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new { Id = id });
+            }
+        }
+    }
+}
